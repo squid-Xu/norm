c88adbd805adc812bd4331e6e6d05470329fd730
徐梦强 <113534980@qq.com>
2024-09-15 22:49:05 +0800

    feat：自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,22 @@ import vue from '@vitejs/plugin-vue';
 // 引入path模块
 import path from 'path';
 
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		AutoImport({
+			// 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+			imports: ['vue'],
+			dts: path.resolve(path.resolve(__dirname, 'src'), 'auto-imports.d.ts'), // 指定自动导入函数TS类型声明文件路径
+		}),
+		Components({
+			dts: path.resolve(path.resolve(__dirname, 'src'), 'components.d.ts'), // 指定自动导入组件TS类型声明文件路径
+		}),
+	],
 	//路径别名
 	resolve: {
 		alias: {
