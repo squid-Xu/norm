18d5421003d90cc0c07211144d406fcbaa5cb740
徐梦强 <1135349080@qq.com>
2024-09-18 09:01:15 +0800

    安装element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,9 +16,11 @@ export default defineConfig({
 			// 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
 			imports: ['vue'],
 			dts: path.resolve(path.resolve(__dirname, 'src'), 'auto-imports.d.ts'), // 指定自动导入函数TS类型声明文件路径
+			resolvers: [ElementPlusResolver()],
 		}),
 		Components({
-			dts: path.resolve(path.resolve(__dirname, 'src'), 'components.d.ts'), // 指定自动导入组件TS类型声明文件路径
+			dts: path.resolve(path.resolve(__dirname, 'src'), 'components.d.ts'), // 指定自动导入组件TS类型声明文件路径,
+			resolvers: [ElementPlusResolver()],
 		}),
 	],
 	//路径别名
